719ed22ab70a37c8f7cd1cda4f40c8dc2121bd04
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2023-07-23 20:30:39 -0400

    Created CoreLoop Template

---
+++ src/Server/Modules/States/InProgress.lua
@@ -0,0 +1,52 @@
+-- InProgress
+-- Author(s): Jesse Appleton
+-- Date: 07/23/2023
+
+--[[
+    
+]]
+
+---------------------------------------------------------------------
+
+-- Constants
+
+-- Knit
+local Knit = require( game:GetService("ReplicatedStorage"):WaitForChild("Knit") )
+local Janitor = require( Knit.Util.Janitor )
+local Promise = require( Knit.Util.Promise )
+
+-- Modules
+local CoreLoopService = Knit.GetService("CoreLoopService")
+
+local Round = require( Knit.Modules.Round )
+
+-- Roblox Services
+
+-- Variables
+
+-- Objects
+
+---------------------------------------------------------------------
+
+
+local InProgress = {}
+InProgress.__index = InProgress
+
+
+function InProgress.new(): ( {} )
+    local self = setmetatable( {}, InProgress )
+    self._janitor = Janitor.new()
+
+    self._round = Round.new()
+
+    self._janitor:Add( self._round, "Destroy")
+    return self
+end
+
+
+function InProgress:Destroy(): ()
+    self._janitor:Destroy()
+end
+
+
+return InProgress
---
+++ src/Server/Services/Game/CoreLoopService.lua
@@ -0,0 +1,57 @@
+-- CoreLoopService
+-- Author(s): Jesse Appleton
+-- Date: 07/23/2023
+
+--[[
+    
+]]
+
+---------------------------------------------------------------------
+
+-- Constants
+
+-- Knit
+local Knit = require( game:GetService("ReplicatedStorage"):WaitForChild("Knit") )
+local RemoteSignal = require( Knit.Util.Remote.RemoteSignal )
+
+-- Modules
+local Enums = require( Knit.SharedModules.Enums )
+
+local StatesFolder: Folder = Knit.Modules.States
+
+-- Roblox Services
+
+-- Variables
+
+-- Objects
+
+---------------------------------------------------------------------
+
+
+local CoreLoopService = Knit.CreateService {
+    Name = "CoreLoopService";
+    Client = {
+        UpdatePlayerCash = RemoteSignal.new()
+    };
+}
+
+
+function CoreLoopService:KnitStart(): ()
+    self:SetState(Enums.State.InProgress)
+end
+
+function CoreLoopService:SetState( stateName: string ): ()
+
+    if( self.State ) then
+        self.State:Destroy()
+    end
+
+    self.State = require( StatesFolder[stateName] ).new()
+end
+
+function CoreLoopService:KnitInit(): ()
+    
+end
+
+
+return CoreLoopService
