27f3349ed6c215043466b00cd7ca34ef7b061889
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2023-07-24 16:49:59 -0400

    Added BaseHealth to HUD

--- src/Client/Controllers/UI/HUD.lua
+++ src/Client/Controllers/UI/HUD.lua
@@ -18,6 +18,7 @@ local Knit = require( game:GetService("ReplicatedStorage"):WaitForChild("Knit") 
 local HUDCash = require( Knit.Modules.UI.HUDCash )
 local WeaponShopButtonClass = require( Knit.Modules.UI.WeaponShopButton )
 local WeaponShopClass = require( Knit.Modules.UI.WeaponShop )
+local BaseHealthClass = require( Knit.Modules.UI.BaseHealth )
 
 -- Roblox Services
 
@@ -28,7 +29,7 @@ local SideDisplay: Frame = Knit.MainUI:WaitForChild("SideDisplay")
 local CoinsDisplay: Frame = SideDisplay:WaitForChild("CoinsDisplay")
 local WeaponShopButton: TextButton = Knit.MainUI:WaitForChild("WeaponShopButton")
 local WeaponShop: Frame = Knit.MainUI:WaitForChild("WeaponShop")
-
+local BaseHealth: Frame = Knit.MainUI:WaitForChild("BaseHealth")
 ---------------------------------------------------------------------
 
 local HUD = Knit.CreateController { Name = "HUD" }
@@ -40,7 +41,7 @@ function HUD:KnitStart(): ()
     HUDCash.new(CoinsDisplay)
     WeaponShopButtonClass.new(WeaponShopButton)
     WeaponShopClass.new(WeaponShop)
-
+    BaseHealthClass.new(BaseHealth)
 end
 
 
